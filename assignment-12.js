ab89720aa6b72d165c2be4acca28c2d23429fcc9
mowe1307 <mowe1307@colorado.edu>
2022-04-07 16:32:00 -0600

    m

---
+++ assignment-12.js
@@ -0,0 +1,22 @@
+async function getQuote() {
+  console.log("quote button was clicked");
+  try {
+    const response = await fetch(endpoint);
+    if (!response.ok) {
+      throw Error(response.statusText)
+    }
+    const json = await response.json();
+    console.log(json);
+    displayQuote(json.url);
+  }catch(err) {
+    console.log(err)
+    alert('no ducks for you');
+  }
+}
+function displayQuote(quote) {
+  const quoteText = document.querySelector('#js-quote-text');
+  quoteText.textContent = quote;
+}
+const endpoint = 'https://random-d.uk/api/v2/random';
+const quoteButton = document.querySelector('#js-new-quote');
+quoteButton.addEventListener('click', getQuote);
