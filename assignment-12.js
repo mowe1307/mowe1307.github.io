4d75e1bef7ef9dbac790de24cb7c95b111c7cb41
mowe1307 <mowe1307@colorado.edu>
2022-04-10 18:54:23 -0600

    c

--- assignment-12.js
+++ assignment-12.js
@@ -1,18 +1,23 @@
-async function getQuote() {
-  console.log("quote button was clicked");
-  try {
-    const response = await fetch(endpoint,{mode: 'no-cors'});
+//async function getQuote() {
+  //console.log("quote button was clicked");
+  //try {
+   //const response = await fetch(endpoint,{mode: 'no-cors'});
     //if (!response.ok) {
-    //  throw Error(response.statusText)
+     //throw Error(response.statusText)
     //}
-    console.log(response);
-    const json = await response.json();
-    console.log(json);
-    displayQuote(json.url);
-  }catch(err) {
-    console.log(err)
-    alert('no ducks for you');
-  }
+
+    //console.log(response);
+    //const json = await response.json();
+    //console.log(json);
+   //displayQuote(json.url);
+  //}catch(err) {
+    //console.log(err)
+   //alert('no ducks for you');
+ //}
+//}
+function getQuote() {
+  const image = document.querySelector(‘img’);
+  image.src = 'https://random-d.uk/api/randomimg?t=' '+ new Date().getTime();
 }
 function displayQuote(quote) {
   const quoteText = document.querySelector('#js-quote-text');
